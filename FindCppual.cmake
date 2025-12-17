1aa8a6800779c29d7239e83e3bff61ec3f756cd6
The Insidious One <fymfifa@gmail.com>
2025-12-17 09:32:11 +0200

    updated cppual find file & compiler config;

--- FindCppual.cmake
+++ FindCppual.cmake
@@ -11,7 +11,7 @@ find_path(CPPUAL_INCLUDE_DIR
             
 find_path(CPPUAL_LIBRARIES_DIR
     NAMES
-        libcppual-host.so cppual-host.dll cppual-host
+        libcppual-endoskeleton.so cppual-endoskeleton.dll cppual-endoskeleton
     PATHS
         ${CMAKE_CURRENT_SOURCE_DIR}/../cppual/bin/${CMAKE_BUILD_TYPE}/${CMAKE_SYSTEM_NAME}-${CMAKE_CXX_COMPILER_ID}-${CMAKE_SYSTEM_PROCESSOR}
         ${CMAKE_CURRENT_SOURCE_DIR}/../build-cppual-Desktop-${CMAKE_BUILD_TYPE}
